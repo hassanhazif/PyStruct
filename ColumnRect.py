c4edf7e1ccf422a3fd1752f1f67dd45ad864f645
Hassan Hazif Shakir <hassanhazif@gmail.com>
2025-01-22 01:03:24 +0500

    Started column class

---
+++ ColumnRect.py
@@ -0,0 +1,12 @@
+from Prelims import LoadData
+from math import ceil
+from math import pi
+from math import tan
+from math import cos
+from math import sin
+from math import radians
+from Prelims import cot
+
+class RCC_ColumnRect:
+    def __init__ (self, name: str):
+        self.name = name
